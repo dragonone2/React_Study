ac48d37945b3cf61d75865532b7da3479aeb4b5d
kanghyunho <dragonone2@naver.com>
2023-11-14 14:27:51 +0900

    11/14

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='black-nav'>
+      <h4>블로그임</h4>
+     </div>
+     <div className='list'>
+      <h4>글제목</h4>
+      <p>11월14일 발행</p>
+     </div>
     </div>
   );
 }
