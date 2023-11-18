d0766d6e7f19e920923410055974d1be3475bac1
kanghyunho <dragonone2@naver.com>
2023-11-18 17:19:34 +0900

    state및props

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let[modal, setModal] = useState(false);
-
+  let[title, setTitle] = useState(0);
   return (
     <div className="App">
      <div className='black-nav'>
@@ -29,32 +29,21 @@ function App() {
       글제목변경(정렬);
       }}>정렬</button>
 
-
-     {/* <div className='list'>
-      <h4>{글제목[0]} <span onClick={ ()=>{c(따봉+1)}}>👍</span> {따봉} </h4>
-      <p>11월14일 발행</p>
-     </div>
-
-     <div className='list'>
-      <h4>{글제목[1]}</h4>
-      <p>11월14일 발행</p>
-     </div>
-
-     <div className='list'>
-      <h4 onClick={()=>{setModal(true)}}>{글제목[2]}</h4>
-      <p>11월14일 발행</p>
-     </div> */}
-
       {
         글제목.map(function(a, i){ //map 반복분이 돌때마다 a라는 파라미터가 State배열 안에 데이터를 받는다, i는 0부터1씩 증가함 총 2개의 파라마티 설정 가능 
           return (
             <div className='list' key={i}>
-            <h4 onClick={()=>{setModal(true)}}>{글제목[i]}
+            <h4 onClick={()=>{
+              setModal(true)
+              setTitle(i)
+              }}>{글제목[i]}
+           
             <span onClick={()=>{
               let copy = [...따봉];
               copy[i] = copy[i] + 1;
               따봉변경(copy)
             }}>👍</span> {따봉[i]} 
+            
             </h4>
             <p>11월14일 발행</p>
            </div>
@@ -64,28 +53,18 @@ function App() {
 
 
       { 
-      modal == true ? <Modal/> : null //이 코드는 if문 ( 조건 ? 참일떄 : 거짓일때)
+      modal == true ? <Modal 변경 = "여자코트 추천" title = {title} 글제목={글제목}/> : null //이 코드는 if문 ( 조건 ? 참일떄 : 거짓일때)
       }
     </div>
   );
-
-
-  // const Modal = () => {
-  //   return(
-  //     <div className='modal'>
-  //       <h4>제목</h4>
-  //       <p>날짜</p>
-  //       <p>상세내용</p>
-  //     </div>
-  //   )
-  // }  밑의 함수랑 같은 의미
-
-  function Modal(){
+  
+  function Modal(props){
     return(
       <div className='modal'>
-        <h4>제목</h4>
+        <h4>{props.글제목[props.title]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button>글수정</button>
       </div>
     )
   }
